b8cc7a2eaf8f98494dd755dfedbaca9c81cab2ba
VladyslavProtchenko <vladyslav25cm@gmail.com>
2026-01-24 02:46:59 +0200

    added fix for vercel to launch website

--- app/[locale]/profile/favorites/page.tsx
+++ app/[locale]/profile/favorites/page.tsx
@@ -2,6 +2,8 @@
 import { getProductsByCategory } from "@/services/getProducts";
 import FavoritesList from "./FavoritesList";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Favorites() {
   const products = await getProductsByCategory();
 
