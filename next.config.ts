4d2f30cd2c045f42b817a41d7eea70386a35faab
VladyslavProtchenko <vladyslav25cm@gmail.com>
2026-01-26 16:51:46 +0200

    fix bugs with images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,18 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig: NextConfig = {
   images: {
     unoptimized: process.env.NODE_ENV === 'development',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/serce-oceanu-71e48.firebasestorage.app/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
