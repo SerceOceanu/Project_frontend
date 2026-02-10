109631105cca848be7ea4236a3f806e296e5e3c7
VladyslavProtchenko <vladyslav25cm@gmail.com>
2026-02-10 03:39:09 +0200

    hide empty section

--- app/components/home-page/CategorySection.tsx
+++ app/components/home-page/CategorySection.tsx
@@ -3,6 +3,7 @@ import ProductsSlider from "../productsSlider";
 
 export default async function CategorySection({ category, title, href }: { category: string, title: string, href: string }) {
   const products = await getProductsByCategory(category);
+  if (!products || products.length === 0) return null;
   return (
     <section className="container flex flex-col mx-auto px-4 md:px-10 xl:px-[100px] ">
         <h2 className="rubik text-3xl font-bold mb-10">{title}</h2>
